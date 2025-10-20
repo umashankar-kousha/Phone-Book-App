872dd5bac313a13d89cc211e26a180aa8eef81e7
uma shankar <umashankar.kousha@gmail.com>
2025-10-21 03:32:38 +0530

    all operations implemented

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ let mobileBtnEl = document.getElementById("mobileBtn");
 let pcBtnEl = document.getElementById("pcBtn");
 let saveBtnEl = document.getElementById("saveBtn");
 let formSectionEl = document.getElementById("formSection");
+let updateBtnEl = document.getElementById("updateBtn");
+let formNameEl = document.getElementById("formName");
+let formNumberEl = document.getElementById("formNumber");
+let SearchContactEl = document.getElementById("SearchContact");
+
 let API = "https://phonebook-server-m40k.onrender.com";
 let allContacts = [];
+let contactId = "";
 
 function toggleElements(element) {
   homeSectionEl.classList.add("text-hide");
@@ -21,7 +27,23 @@ pcBtnEl.addEventListener("click", (event) => {
   toggleElements(event.target);
 });
 
-saveBtnEl.addEventListener("click", (event) => {
+function onEditContact(id) {
+  contactId = id;
+  homeSectionEl.classList.add("text-hide");
+  formSectionEl.classList.remove("hide");
+  if (window.innerWidth >= 800) {
+    pcBtnEl.classList.add("hide");
+  } else {
+    mobileBtnEl.classList.add("hide");
+  }
+  saveBtnEl.classList.add("hide");
+  updateBtnEl.classList.remove("hide");
+  let contact = allContacts.find((item) => item._id === id);
+  formNameEl.value = contact.fullName;
+  formNumberEl.value = contact.phoneNumber;
+}
+
+saveBtnEl.addEventListener("click", async (event) => {
   event.preventDefault();
   homeSectionEl.classList.remove("text-hide");
   formSectionEl.classList.add("hide");
@@ -30,12 +52,73 @@ saveBtnEl.addEventListener("click", (event) => {
   } else {
     pcBtnEl.classList.remove("hide");
   }
+  const fullName = formNameEl.value;
+  const phoneNumber = formNumberEl.value;
+
+  try {
+    const response = await fetch(`${API}/api/save/contact`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ fullName, phoneNumber }),
+    });
+    if (response) {
+      const data = await response.json();
+      alert(data.message);
+      getAllContacts();
+      document.querySelector("form").reset();
+    }
+  } catch (error) {
+    console.error(error);
+    alert(error.message);
+  }
+});
+
+async function updateContact(fullName, phoneNumber) {
+  try {
+    const response = await fetch(`${API}/api/update/contact/${contactId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ fullName, phoneNumber }),
+    });
+    if (response) {
+      const data = await response.json();
+      alert(data.message);
+      getAllContacts();
+      document.querySelector("form").reset();
+    }
+  } catch (error) {
+    console.error(error);
+    alert(error.message);
+  }
+}
+
+updateBtnEl.addEventListener("click", (event) => {
+  event.preventDefault();
+  homeSectionEl.classList.remove("text-hide");
+  formSectionEl.classList.add("hide");
+  if (window.innerWidth >= 800) {
+    pcBtnEl.classList.remove("hide");
+  } else {
+    mobileBtnEl.classList.remove("hide");
+  }
+  saveBtnEl.classList.remove("hide");
+  updateBtnEl.classList.add("hide");
+
+  let contactName = formNameEl.value;
+  let contactNumber = formNumberEl.value;
+
+  updateContact(contactName, contactNumber);
 });
 
 function renderContacts() {
   allContacts.forEach((element) => {
-    let name = element.fullName;
-    let phoneNumber = element.phoneNumber;
+    const name = element.fullName;
+    const phoneNumber = element.phoneNumber;
+    const elementId = element._id;
     contactContainerEl.innerHTML += `<li class="contact-card">
             <div class="initial">
               <span>${name[0].toUpperCase()}</span>
@@ -45,10 +128,10 @@ function renderContacts() {
               <p class="number">${phoneNumber}</p>
             </div>
             <div class="contact-operations-container">
-              <button class="operations-btn">
-                <span class="material-symbols-outlined"> edit </span>
+              <button class="operations-btn" onclick=onEditContact('${elementId}')>
+                <span class="material-symbols-outlined" > edit </span>
               </button>
-              <button class="operations-btn">
+              <button class="operations-btn" onclick=onDeleteContact('${elementId}')>
                 <span class="material-symbols-outlined"> delete </span>
               </button>
             </div>
@@ -58,15 +141,55 @@ function renderContacts() {
 
 async function getAllContacts() {
   try {
-    let response = await fetch(`${API}/api/all/contacts`);
-    let data = await response.json();
+    const response = await fetch(`${API}/api/all/contacts`);
+    const data = await response.json();
     allContacts = data;
 
     console.log(allContacts);
+    contactContainerEl.innerHTML = "";
     renderContacts();
   } catch (error) {
     console.error(error);
   }
 }
 
+async function onDeleteContact(delId) {
+  try {
+    const response = await fetch(`${API}/api/delete/contact/${delId}`, {
+      method: "DELETE",
+    });
+    if (response) {
+      const data = await response.json();
+      alert(data.message);
+      getAllContacts();
+    }
+  } catch (error) {
+    console.error(error);
+    alert(error.message);
+  }
+}
+
 getAllContacts();
+
+SearchContactEl.addEventListener("search", (event) => {
+  const searchtext = SearchContactEl.value;
+  if (searchtext == "") {
+    getAllContacts();
+  } else {
+    const filteredContacts = allContacts.filter((element) => {
+      return (
+        element.fullName.toLowerCase().includes(searchtext) ||
+        element.phoneNumber.toString().includes(searchtext)
+      );
+    });
+    allContacts = filteredContacts;
+    contactContainerEl.innerHTML = "";
+    renderContacts();
+  }
+});
+
+SearchContactEl.addEventListener("blur", (event) => {
+  if (event.target.value == "") {
+    getAllContacts();
+  }
+});
