a376661bbd771b38bc3a4ba6d8b20890f4cca91a
uma shankar <umashankar.kousha@gmail.com>
2025-10-20 23:54:42 +0530

    all contacts at loading api end point implemented

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 let homeSectionEl = document.getElementById("homeSection");
+let contactContainerEl = document.getElementById("contactContainer");
 let mobileBtnEl = document.getElementById("mobileBtn");
 let pcBtnEl = document.getElementById("pcBtn");
 let saveBtnEl = document.getElementById("saveBtn");
 let formSectionEl = document.getElementById("formSection");
+let API = "https://phonebook-server-m40k.onrender.com";
+let allContacts = [];
 
 function toggleElements(element) {
   homeSectionEl.classList.add("text-hide");
@@ -28,3 +31,42 @@ saveBtnEl.addEventListener("click", (event) => {
     pcBtnEl.classList.remove("hide");
   }
 });
+
+function renderContacts() {
+  allContacts.forEach((element) => {
+    let name = element.fullName;
+    let phoneNumber = element.phoneNumber;
+    contactContainerEl.innerHTML += `<li class="contact-card">
+            <div class="initial">
+              <span>${name[0].toUpperCase()}</span>
+            </div>
+            <div>
+              <p class="name">${name}</p>
+              <p class="number">${phoneNumber}</p>
+            </div>
+            <div class="contact-operations-container">
+              <button class="operations-btn">
+                <span class="material-symbols-outlined"> edit </span>
+              </button>
+              <button class="operations-btn">
+                <span class="material-symbols-outlined"> delete </span>
+              </button>
+            </div>
+          </li>`;
+  });
+}
+
+async function getAllContacts() {
+  try {
+    let response = await fetch(`${API}/api/all/contacts`);
+    let data = await response.json();
+    allContacts = data;
+
+    console.log(allContacts);
+    renderContacts();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getAllContacts();
